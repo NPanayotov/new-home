968d5e606f9b12c75ebaf783745ce499ae2f2def
Nate <npanajotov@2create.bg>
2023-09-29 16:38:51 +0300

    Fix mobile colors

--- src/js/modules/animate-on-scroll.js
+++ src/js/modules/animate-on-scroll.js
@@ -1,10 +1,16 @@
-import { $window, $body } from '../utils/globals'
+import { $window } from '../utils/globals'
 
 const observerOptions = {
 	rootMargin: '0px',
 	threshold: 0.4,
 }
 
+/**
+ * Mutation Observer CB
+ * @param  {NodeList} entries
+ * @param  {obj} observer
+ * @return {Void}
+ */
 const observerCallback = (entries, observer) => {
 	entries.forEach(entry => {
 		const isVisible = entry.isIntersecting;
@@ -18,7 +24,6 @@ const observer = new IntersectionObserver(observerCallback, observerOptions);
 const animatedElements = document.querySelectorAll('.animate-in')
 
 $window.on('load', () => {
-
 	for (const element of animatedElements) {
 		observer.observe(element);
 	}
