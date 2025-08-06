7b62a354846cc55604e10229b67d25b37554ef2f
Nate <npanajotov@2create.bg>
2025-08-06 16:39:09 +0300

    Fix forms js loading

--- src/js/modules/handle-contact-form.js
+++ src/js/modules/handle-contact-form.js
@@ -1,47 +1,60 @@
-emailjs.init('EqGMiwsB7jtM_fDAn');
+const initEmailJs = () => {
+	let emailjs = window.emailjs;
 
-const formName = 'Eurobuld';
-const replayTo = 'r-grozev@abv.bg';
-
-$('.js-contact-form').on('submit', 'form', function(evt) {
-	const $this = $(this);
-	const formData = $this.serializeArray();
-	const dataObj = {};
-	const $formContainer = $this.parent();
-	const $honeyPot = $this.find('.js-hp');
-
-	if ($honeyPot.val() !== '') {
-		evt.preventDefault();
-		return; //Robot detected
-	};
-
-	$formContainer.addClass('is-sending');
-
-	for (const item of formData) {
-		const { name, value } = item;
-
-		dataObj[name] = value;
+	if (emailjs === undefined) {
+		return;
 	}
 
-	const { name: sender_name, email: sender_email, phone: sender_phone, msg: message } = dataObj;
+	emailjs.init('EqGMiwsB7jtM_fDAn');
 
-	emailjs.send("contact_service","contact_form",{
-		from_name: formName,
-		sender_name,
-		sender_email,
-		sender_phone,
-		message,
-		reply_to: replayTo,
-	}) .then(function() {
+	const formName = 'Eurobuld';
+	const replayTo = 'r-grozev@abv.bg';
 
-		$formContainer.addClass('has-sent');
+	$('.js-contact-form').on('submit', 'form', function(evt) {
+		const $this = $(this);
+		const formData = $this.serializeArray();
+		const dataObj = {};
+		const $formContainer = $this.parent();
+		const $honeyPot = $this.find('.js-hp');
 
-		console.log('SUCCESS!');
-	}, function(error) {
-		console.log('FAILED...', error);
+		if ($honeyPot.val() !== '') {
+			evt.preventDefault();
+			return; //Robot detected
+		};
 
-		$formContainer.addClass('has-error');
-	});
+		$formContainer.addClass('is-sending');
 
-	evt.preventDefault();
-})
+		for (const item of formData) {
+			const { name, value } = item;
+
+			dataObj[name] = value;
+		}
+
+		const { name: sender_name, email: sender_email, phone: sender_phone, msg: message } = dataObj;
+
+		emailjs.send("contact_service","contact_form",{
+			from_name: formName,
+			sender_name,
+			sender_email,
+			sender_phone,
+			message,
+			reply_to: replayTo,
+		}) .then(function() {
+
+			$formContainer.addClass('has-sent');
+
+			console.log('SUCCESS!');
+		}, function(error) {
+			console.log('FAILED...', error);
+
+			$formContainer.addClass('has-error');
+		});
+
+		evt.preventDefault();
+	})
+
+}
+
+
+initEmailJs();
+
